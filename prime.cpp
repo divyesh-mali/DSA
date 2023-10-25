f8596c4c2d8bcf6f27a7fe00c64f3eb875559d6c
divyesh-mali <medivyeshmali@gmail.com>
2023-10-25 20:37:47 +0530

    Optimised Code for Prime

--- prime.cpp
+++ prime.cpp
@@ -1,62 +1,30 @@
-// UNDERSTAND THIS CODE PROPERLY !!!
+#include <iostream>
+using namespace std;
 
-#include <iostream>  
-using namespace std;  
-int main()  
-{  
-  int n, i, m=0, flag=0;  
-  cout << "Enter the Number to check Prime: ";  
-  cin >> n;  
-  m=n/2;  
-  for(i = 2; i <= m; i++)  
-  {  
-      if(n % i == 0)  
-      {  
-          cout<<"Number is not Prime."<<endl;  
-          flag=1;  
-          break;  
-      }  
-  }  
-  if (flag==0)  
-      cout << "Number is Prime."<<endl;  
-  return 0;  
-} 
+int main(){
+    int n;
+    int m=0;
+    bool flag = true;
 
+    cout << "Enter Number: ";
+    cin >> n;
 
+    m = n/2;
 
+    for(int i=2; i<=m; i++){ // best case if number is found to be divisible by 2
+        if(n%i == 0){        // worst case if number is found to be divisible by m
+            flag = false;
+            cout << i << "\n";
+            break;
+        }
+    }
 
+    if(flag == true){
+        cout << "Number is Prime!";
+    }
+    else{
+        cout << "Number is Not Prime!";
+    }
 
-
-
-
-
-
-// #include<iostream>
-// using namespace std;
-// int main()
-// {
-//     int n, div = 2;
-//     cout << "Enter a number: ";
-//     cin >> n;
-
-//     while(div<2)
-//     {
-//         if(n % div==0)
-//         {
-//             cout <<"The number is not prime !!";
-//         }
-
-//         else
-//         {
-//             div = div+1;
-//         }
-
-//         cout << "Number is prime :)";
-//     }
-
-
-//     return 0;
-// }
-
-
- 
+    return 0;
+}
