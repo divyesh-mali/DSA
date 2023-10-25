2c8286312ec153d2e9deba891660cbe5c7d58029
divyesh-mali <medivyeshmali@gmail.com>
2023-10-25 20:38:25 +0530

    Learned Datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int a=123;
+    // cout << a << endl;
+
+    // char b = 'z';
+    // cout << b << endl;
+
+    // bool bl = true;
+    // cout << bl << endl;
+
+    // float f=1.2;
+    // cout << f << endl;
+
+    // double d = 1.23;
+    // cout << d << endl;
+
+    // int size = sizeof(a);
+    // cout << "size of a: " << size << endl;
+
+
+    char ch1 = 69;
+    cout << ch1 << endl; // based on ASCII values
+
+    char ch2 = 'F';
+    cout << ch2 << endl; // based on ASCII values
+
+    return 0;
+}
